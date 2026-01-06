65e9ec7061820aaf384d5d31182d056eef7332ac
Oscar Ruiz <oscaralexruiz@gmail.com>
2026-01-06 09:03:03 -0500

    fix: make dark mode default

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -46,8 +46,7 @@ export default async function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="dark"
           disableTransitionOnChange>
           {children}
         </ThemeProvider>
