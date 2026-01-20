81250ff1de3033905a16a3cd745c2763baaae01e
Oscar Ruiz <oscaralexruiz@gmail.com>
2026-01-19 21:32:39 -0500

    --wip-- [skip ci]

---
+++ app/_db/index.ts
@@ -0,0 +1,8 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import env from '@/env/server';
+
+// Disable prefetch as it is not supported for "Transaction" pool mode
+const client = postgres(env.DATABASE_URL, { prepare: true });
+
+export const db = drizzle(client);
