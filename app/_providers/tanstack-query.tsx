2e1240ea542b8969d93cf8e0aa2ce7c0de14295f
Cristian Santos <cristian@hey.com>
2026-01-26 21:59:59 -0500

    feat: persistency of the word after locking the phone

--- app/_providers/tanstack-query.tsx
+++ app/_providers/tanstack-query.tsx
@@ -3,7 +3,13 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import type { FC, PropsWithChildren } from 'react';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export const TanstackQueryProvider: FC<PropsWithChildren> = ({ children }) => {
   return (
